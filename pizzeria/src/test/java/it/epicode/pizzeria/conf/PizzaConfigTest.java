4774250b23c1537338268a03b0eeab92bc418e29
Michele Mandanici <michimandanici@tim.it>
2025-02-05 17:45:42 +0100

    test

---
+++ pizzeria/src/test/java/it/epicode/pizzeria/conf/PizzaConfigTest.java
@@ -0,0 +1,4 @@
+package it.epicode.pizzeria.conf;
+
+public class PizzaConfigTest {
+}
---
+++ pizzeria/src/test/java/it/epicode/pizzeria/ordineTest/OrdineTest.java
@@ -0,0 +1,19 @@
+package it.epicode.pizzeria.ordineTest;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class OrdineTest {
+
+    @Test
+    @DisplayName("test di creazione ordine")
+    public void testOrdine(){
+
+        int numeroOrdine = 1;
+
+    }
+
+}
